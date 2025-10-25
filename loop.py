920e7653c108dfb2e1ae4ce4b5723d226e562d01
Ira Goldstein <igoldstein@siena.edu>
2025-10-25 18:05:11 -0400

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,16 @@
+# loop.py
+
+# Repeat Text 4 Times
+# CSIS 110
+# Ira Goldstein
+
+# Takes text as a parameter 
+# Prints that text four times
+def repeat(name):
+  for index in range (0, 4):
+    print(name)
+
+#  Code goes above
+#  Call our code below
+repeat("How now brown cow?)
+       
